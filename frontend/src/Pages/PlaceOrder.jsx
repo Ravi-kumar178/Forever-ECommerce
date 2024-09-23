1a29c43143b1ce043bb7d0204178a21bf04dbed7
Ravi <bhardwajravi2025@gmail.com>
2024-09-24 01:26:28 +0530

    cart/placeorder

--- frontend/src/Pages/PlaceOrder.jsx
+++ frontend/src/Pages/PlaceOrder.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import Title from '../Components/Common/Title'
+import CartTotal from '../Components/Cart/CartTotal'
+import { assets } from '../assets/assets'
+import { ShopContext } from '../Context/ShopContext'
 
 const PlaceOrder = () => {
+
+  const[paymentMethod, setPaymentMethod] = useState('cod');
+  const {navigate} = useContext(ShopContext);
+
   return (
-    <div>PlaceOrder</div>
+    <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
+      {/* left side */}
+      <div className="flex flex-col gap-4 w-full max-w-[480px]">
+        <div className=' text-xl sm:text-2xl my-3'>
+          <Title text1={'Delivery'} text2={'Information'}/>
+        </div>
+
+        <div className="flex gap-3">
+          <input type="text" placeholder='First Name' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+          <input type="text" placeholder='Last Name' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+        </div>
+        <input type="email" placeholder='Email Address' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+        <input type="text" placeholder='Street' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+
+        <div className="flex gap-3">
+          <input type="text" placeholder='City' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+          <input type="text" placeholder='State' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+        </div>
+        <div className="flex gap-3">
+          <input type="number" placeholder='Zip code' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+          <input type="text" placeholder='Country' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+        </div>
+        <input type="number" placeholder='Phone' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+      </div>
+
+      {/* right side */}
+      <div className='mt-8'>
+        <div className="mt-8 min-w-80">
+          <CartTotal/>
+        </div>
+
+         {/* payment method */}
+         <div className="mt-12">
+          <Title text1={'PAYMENT'} text2={'METHODS'}/>
+
+          <div className='flex gap-3 flex-col lg:flex-row'>
+            <div onClick={()=>setPaymentMethod('stripe')} className={`flex items-center gap-3 border p-2 px-3 cursor-pointer`}>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${paymentMethod==='stripe'?"bg-green-400":""}`}></p>
+              <img src={assets.stripe_logo} loading='lazy' className=' h-5 mx-4' alt="" />
+            </div>
+            <div onClick={()=>setPaymentMethod('razorpay')} className={`flex items-center gap-3 border p-2 px-3 cursor-pointer`}>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${paymentMethod==='razorpay'?"bg-green-400":""}`}></p>
+              <img src={assets.razorpay_logo} loading='lazy' className=' h-5 mx-4' alt="" />
+            </div>
+            <div onClick={()=>setPaymentMethod('cod')} className={`flex items-center gap-3 border p-2 px-3 cursor-pointer`}>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${paymentMethod==='cod'?"bg-green-400":""}`}></p>
+              <p className=' text-gray-500 text-sm font-medium mx-4 '>CASH ON DELIEVERY</p>
+            </div>
+          </div>
+
+          <div className=' w-full text-end mt-8'>
+            <button onClick={()=>navigate("/orders")} className=' bg-black text-white px-16 py-3 text-sm'>Place Order</button>
+          </div>
+         </div>
+      </div>
+    </div>
   )
 }
 
