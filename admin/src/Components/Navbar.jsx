51ac6488438b12e7b5c6f07c9a617afc79ce764e
Ravi <bhardwajravi2025@gmail.com>
2024-09-28 13:14:20 +0530

    adminlogin

--- admin/src/Components/Navbar.jsx
+++ admin/src/Components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {assets} from '../assets/assets'
+import { toast } from 'react-toastify';
 
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center justify-between py-2 px-[4%]'>
         <img src={assets.logo} loading='lazy' className='w-[max(10%,80px)]' alt="" />
-        <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+        <button onClick={()=>{setToken('');toast.success("Logged out")}} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }
