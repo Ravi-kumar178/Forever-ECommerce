f69265565210b0d48d59337c50be03afea55396e
Ravi <bhardwajravi2025@gmail.com>
2024-10-01 12:41:21 +0530

    deploymentbugfixing

--- frontend/src/Components/Common/ProductItem.jsx
+++ frontend/src/Components/Common/ProductItem.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { ShopContext } from '../../Context/shopContext'
+import { ShopContext } from '../../Context/ShopContext'
 import {Link} from "react-router-dom"
 
 const ProductItem = ({id,name,image,price}) => {
